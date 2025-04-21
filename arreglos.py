b74563e081542102df37494c88c7f17b4ca0929f
Tzequiel <richardhernandezbarraza@gmail.com>
2025-04-21 13:30:47 -0400

    primera parte

--- arreglos.py
+++ arreglos.py
@@ -1,1 +1,12 @@
-#comentando xd
+#comentando xdgti add
+frutas = ["manzana", "pera", "frutilla"]
+print(frutas[0])
+
+########aaaaaaa###########
+
+estudiantes =[
+    {"nombre" : "Maxi", "edad": 20, "curso" : "2a"},
+    {"nombre" : "Richi", "edad": 19, "curso" : "4a"}
+    
+]
+print(estudiantes[1]["nombre"])
